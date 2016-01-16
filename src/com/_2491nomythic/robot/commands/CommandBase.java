47c783aa7d0c305060a35fa91197a9b447c6c373
Lenovo_3 <2491nomythic@gmail.com>
2016-01-16 14:10:50 -0600

    created CommandBase

---
+++ src/com/_2491nomythic/robot/commands/CommandBase.java
@@ -0,0 +1,56 @@
+package com._2491nomythic.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com._2491nomythic.robot.OI;
+import com._2491nomythic.robot.subsystems.*;
+
+/**
+ * The base for all commands. All atomic commands should subclass CommandBase.
+ * CommandBase stores creates and stores each control system. To access a
+ * subsystem elsewhere in your code in your code use
+ * CommandBase.exampleSubsystem
+ * 
+ * @author Author
+ */
+public abstract class CommandBase extends Command {
+	
+	protected static OI oi;
+	protected static Drivetrain drivetrain;
+	
+	public static void init() {
+		drivetrain = Drivetrain.getInstance();
+		// This MUST be here. If the OI creates Commands (which it very likely
+		// will), constructing it during the construction of CommandBase (from
+		// which commands extend), subsystems are not guaranteed to be
+		// yet. Thus, their requires() statements may grab null pointers. Bad
+		// news. Don't move it.
+		oi = new OI();
+		oi.init();
+		
+		// Show what command your subsystem is running on the SmartDashboard
+		SmartDashboard.putData(drivetrain);
+	}
+	
+	/**
+	 * The base for all commands. All atomic commands should subclass CommandBase.
+	 * CommandBase stores creates and stores each control system. To access a
+	 * subsystem elsewhere in your code in your code use
+	 * CommandBase.exampleSubsystem
+	 * @param name The name that shows up on the SmartDashboard in association with any command created using this parameter.
+	 */
+	public CommandBase(String name) {
+		super(name);
+	}
+	
+	/**
+	 * The base for all commands. All atomic commands should subclass CommandBase.
+	 * CommandBase stores creates and stores each control system. To access a
+	 * subsystem elsewhere in your code in your code use
+	 * CommandBase.exampleSubsystem
+	 */
+	public CommandBase() {
+		super();
+	}
+}
