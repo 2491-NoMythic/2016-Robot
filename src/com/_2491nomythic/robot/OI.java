0a8a882523235ebffed0ba0df0ef8b81f0785563
Lenovo_3 <2491nomythic@gmail.com>
2016-01-16 13:21:08 -0600

    set up basic oi structure

--- src/com/_2491nomythic/robot/OI.java
+++ src/com/_2491nomythic/robot/OI.java
@@ -1,8 +1,9 @@
 package com._2491nomythic.robot;
 
-import com._2491nomythic.robot.commands.ExampleCommand;
-
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+
+import com._2491nomythic.robot.settings.Constants;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -16,6 +17,68 @@ public class OI {
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
     
+	private final Joystick[] controllers = new Joystick[2];
+	
+	/**
+	 * Initiates some joysticks and buttons.
+	 */
+	public void init() {
+		controllers[0] = new Joystick(Constants.ControllerOnePort);
+		controllers[1] = new Joystick(Constants.ControllerTwoPort);
+		
+	}
+	
+	/**
+	 * Get a controller
+	 * 
+	 * @param id the ID of the controller. 0 = left or driver, 1 = right or codriver.
+	 * @return the instance of the controller requested
+	 */
+	public Joystick getController(int id) {
+		return controllers[id];
+	}
+	
+	/**
+	 * Get a button from a controller
+	 * 
+	 * @param joystickID
+	 *            The id of the controller. 0 = left or driver, 1 = right or
+	 *            codriver.
+	 * @param axisID
+	 *            The id of the button (for use in getRawButton)
+	 * @return the result from running getRawButton(button)
+	 */
+	public boolean getButton(int joystickID, int buttonID) {
+		return controllers[joystickID].getRawButton(buttonID);
+	}
+	
+	/**
+	 * Get an axis from a controller
+	 * 
+	 * @param joystickID
+	 *            The id of the controller. 0 = left or driver, 1 = right or
+	 *            codriver.
+	 * @param axisID
+	 *            The id of the axis (for use in getRawAxis)
+	 * @return the result from running getRawAxis(axis)
+	 */
+	public double getAxis(int joystickID, int axisID) {
+		return controllers[joystickID].getRawAxis(axisID);
+	}
+	
+	/**
+	 * Get an axis from a controller that is automatically squared and deadzoned
+	 * 
+	 * @param joystickID The id of the controller. 0 = left or driver, 1 = right or driver
+	 * @param axisID The id of the axis (for use in getRawAxis)
+	 * @return the squared, deadzoned result from running getRawAxis
+	 */
+	public double getAxisForDrive(int joystickID, int axisID) {
+		double result = controllers[joystickID].getRawAxis(axisID);
+		result = result * Math.abs(result);
+		return Math.abs(result) > 0.05 ? result : 0;
+	}
+	
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
---
+++ src/com/_2491nomythic/robot/settings/Constants.java
@@ -0,0 +1,7 @@
+package com._2491nomythic.robot.settings;
+
+public class Constants {
+	//Joysticks
+	public static final int ControllerOnePort = 0;
+	public static final int ControllerTwoPort = 0;
+}
