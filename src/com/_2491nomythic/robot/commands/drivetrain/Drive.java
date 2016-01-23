77eb30d47aad41ecdbee7707494256b0cf4d1c22
Adrian <awkirchner@gmail.com>
2016-01-23 11:48:00 -0600

    basic drive code finished

--- src/com/_2491nomythic/robot/commands/drivetrain/Drive.java
+++ src/com/_2491nomythic/robot/commands/drivetrain/Drive.java
@@ -1,15 +1,17 @@
 package com._2491nomythic.robot.commands.drivetrain;
 
-import edu.wpi.first.wpilibj.command.Command;
+import com._2491nomythic.robot.commands.CommandBase;
+import com._2491nomythic.robot.settings.ControllerMap;
 
 /**
  *
  */
-public class Drive extends Command {
+public class Drive extends CommandBase {
+	double leftPower, rightPower;
 
     public Drive() {
         // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(drivetrain);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +20,9 @@ public class Drive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	leftPower = oi.getAxisDeadzoned(ControllerMap.driveController, ControllerMap.driveLeftAxis);
+    	rightPower = oi.getAxisDeadzoned(ControllerMap.driveController, ControllerMap.driveRightAxis);
+    	drivetrain.drive(leftPower, rightPower);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -27,10 +32,12 @@ public class Drive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	drivetrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
