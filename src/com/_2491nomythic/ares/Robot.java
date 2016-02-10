c82ad5a1e8409e42cf0a05e61013b676318f35dc
Adrian <awkirchner@gmail.com>
2016-02-09 19:40:09 -0600

    Changed a thing

--- src/com/_2491nomythic/ares/Robot.java
+++ src/com/_2491nomythic/ares/Robot.java
@@ -32,7 +32,6 @@ public class Robot extends IterativeRobot {
 		CommandBase.init();
         // instantiate the command used for the autonomous period
 		autoChooser = new SendableChooser();
-		Variables.shooterRightToLeftRatio = SmartDashboard.getNumber("Shooter Right to Left Ratio", 1.0);
     }
 	
 	public void disabledPeriodic() {
