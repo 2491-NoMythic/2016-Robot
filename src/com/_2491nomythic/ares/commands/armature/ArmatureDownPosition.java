e8c478067c11e0950a2b364e7a289e50ec94fb72
Adrian <awkirchner@gmail.com>
2016-02-13 14:46:13 -0600

    slightly modified armature position commands' structure

--- src/com/_2491nomythic/ares/commands/armature/ArmatureDownPosition.java
+++ src/com/_2491nomythic/ares/commands/armature/ArmatureDownPosition.java
@@ -14,7 +14,6 @@ public class ArmatureDownPosition extends CommandBase{
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		armature.runMotorToPosition(Constants.armatureDownPositionValue);
-		end();
 	}
 	
 	// Called repeatedly when this Command is scheduled to run
@@ -23,7 +22,7 @@ public class ArmatureDownPosition extends CommandBase{
 	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-	    return false;
+	    return true;
 	}
 	
 	// Called once after isFinished returns true
--- src/com/_2491nomythic/ares/commands/armature/ArmatureLowBarPosition.java
+++ src/com/_2491nomythic/ares/commands/armature/ArmatureLowBarPosition.java
@@ -14,7 +14,6 @@ public class ArmatureLowBarPosition extends CommandBase{
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		armature.runMotorToPosition(Constants.armatureLowBarPositionValue);
-		end();
 	}
 	
 	// Called repeatedly when this Command is scheduled to run
@@ -23,7 +22,7 @@ public class ArmatureLowBarPosition extends CommandBase{
 	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-	    return false;
+	    return true;
 	}
 	
 	// Called once after isFinished returns true
