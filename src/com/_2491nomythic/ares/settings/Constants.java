ae7058389f1990f070dc4cdeb46a4f3be17ff42f
Adrian <awkirchner@gmail.com>
2016-02-13 10:27:13 -0600

    corrected some channel constants

--- src/com/_2491nomythic/ares/settings/Constants.java
+++ src/com/_2491nomythic/ares/settings/Constants.java
@@ -6,28 +6,28 @@ public class Constants {
 	public static final int ControllerTwoPort = 1;
 	
 	//Channels
-	public static final int driveTalonLeft1Channel = 10;
-	public static final int driveTalonLeft2Channel = 20;
-	public static final int driveTalonLeft3Channel = 21;
-	public static final int driveTalonRight1Channel = 12;
-	public static final int driveTalonRight2Channel = 22;
-	public static final int driveTalonRight3Channel = 23;
+	public static final int driveTalonLeft1Channel = 18;
+	public static final int driveTalonLeft2Channel = 17;
+	public static final int driveTalonLeft3Channel = 16;
+	public static final int driveTalonRight1Channel = 11;
+	public static final int driveTalonRight2Channel = 12;
+	public static final int driveTalonRight3Channel = 13;
 	public static final int driveEncoderLeftChannel1 = 24;
 	public static final int driveEncoderLeftChannel2 = 25;
 	public static final int driveEncoderRightChannel1 = 26;
 	public static final int driveEncoderRightChannel2 = 27;
 
-	public static final int intakeChannel = 17;
+	public static final int intakeChannel = 10;
 
-	public static final int armatureChannel = 16;
+	public static final int armatureChannel = 19;
 	public static final int armatureEncoderChannel1 = 28;
 	public static final int armatureEncoderChannel2 = 29;
 	
-	public static final int shooterTalonLeftChannel = 14;
-	public static final int shooterTalonRightChannel = 15;
+	public static final int shooterTalonLeftChannel = 15;
+	public static final int shooterTalonRightChannel = 14;
 	public static final int shooterSolenoidChannel1 = 30;
 	public static final int shooterSolenoidChannel2 = 31;
 	
 	//Computation Constants
-	public static final double driveEncoderToFeet = 32;
+	public static final double driveEncoderToFeet = (Integer) null;
 }
