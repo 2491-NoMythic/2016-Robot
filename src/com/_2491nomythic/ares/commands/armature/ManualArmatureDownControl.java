79d47bf7b9befa069a68d21d6dfe3523dcf1363e
Wellstone56 <mkangas98@gmail.com>
2016-02-15 12:45:21 -0600

    switched limiter switch to up movement

--- src/com/_2491nomythic/ares/commands/armature/ManualArmatureDownControl.java
+++ src/com/_2491nomythic/ares/commands/armature/ManualArmatureDownControl.java
@@ -21,9 +21,6 @@ public class ManualArmatureDownControl extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(armature.armLimitSwitch()) {
-    		armature.set(0);
-    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
