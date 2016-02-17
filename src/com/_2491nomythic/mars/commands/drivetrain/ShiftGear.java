ea0c448f3bb1b1122127a530e7ab287d9c484508
Adrian <awkirchner@gmail.com>
2016-02-16 19:29:39 -0600

    removed requires line in ShiftGear

--- src/com/_2491nomythic/mars/commands/drivetrain/ShiftGear.java
+++ src/com/_2491nomythic/mars/commands/drivetrain/ShiftGear.java
@@ -10,7 +10,6 @@ public class ShiftGear extends CommandBase {
     public ShiftGear() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(drivetrain);
     }
 
     // Called just before this Command runs the first time
