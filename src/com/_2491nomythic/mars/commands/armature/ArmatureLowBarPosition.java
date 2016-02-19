efe6321a333b11b58f590972e2df2ccec73917b1
Adrian <awkirchner@gmail.com>
2016-02-18 18:54:34 -0600

    fixed the armature position commands to actually work

--- src/com/_2491nomythic/mars/commands/armature/ArmatureLowBarPosition.java
+++ src/com/_2491nomythic/mars/commands/armature/ArmatureLowBarPosition.java
@@ -22,7 +22,7 @@ public class ArmatureLowBarPosition extends CommandBase{
 	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-	    return true;
+	    return (armature.getEncoderPosition() == Constants.armatureLowBarPositionValue);
 	}
 	
 	// Called once after isFinished returns true
