07eb5bdc54ca7098ecfebead6736533f384ca0f6
Lenovo 4 <2491nomythic@gmail.com>
2016-02-20 14:27:45 -0600

    Made Limit switch channels

--- src/com/_2491nomythic/mars/commands/autonomous/ShootPickupShoot.java
+++ src/com/_2491nomythic/mars/commands/autonomous/ShootPickupShoot.java
@@ -1,5 +1,12 @@
 package com._2491nomythic.mars.commands.autonomous;
 
+import com._2491nomythic.mars.commands.armature.ArmatureIntakePosition;
+import com._2491nomythic.mars.commands.armature.ArmatureLowBarPosition;
+import com._2491nomythic.mars.commands.armature.ArmatureUpPosition;
+import com._2491nomythic.mars.commands.drivetrain.DriveToPosition;
+import com._2491nomythic.mars.commands.drivetrain.RotateDrivetrain;
+import com._2491nomythic.mars.commands.intake.IntakeBall;
+import com._2491nomythic.mars.commands.shooter.LowerShooter;
 import com._2491nomythic.mars.commands.shooter.RaiseShooter;
 import com._2491nomythic.mars.commands.shooter.RunShooterTime;
 
@@ -11,10 +18,27 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ShootPickupShoot extends Command {
 	RaiseShooter raiseShooter;
 	RunShooterTime shoot;
+	DriveToPosition drive1ft;
+	LowerShooter lowerShooter;
+	RotateDrivetrain rotateNeg90;
+	DriveToPosition drive24ft;
 	RotateDrivetrain rotate90;
+	ArmatureUpPosition armUp;
+	ArmatureIntakePosition armIntake;
+	ArmatureLowBarPosition armLowBar;
+	IntakeBall intakeBall;
+	DriveToPosition drive2ft;
+	DriveToPosition drive19ft;
+	RotateDrivetrain rotateNeg22;
     public ShootPickupShoot() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	raiseShooter = new RaiseShooter();
+    	shoot = new RunShooterTime(5);
+    	drive1ft = new DriveToPosition(0.7, 1);
+    	lowerShooter = new LowerShooter();
+    	
+    	
     }
 
     // Called just before this Command runs the first time
--- src/com/_2491nomythic/mars/settings/Constants.java
+++ src/com/_2491nomythic/mars/settings/Constants.java
@@ -5,6 +5,11 @@ public class Constants {
 	public static final int ControllerOnePort = 0;
 	public static final int ControllerTwoPort = 1;
 	
+	//Digital I/O
+	public static final int armatureLimitSwitchChannel = 0;
+	public static final int intakeLimitSwitchChannel = 1;
+	public static final int intakeLimitSwitchChannel2 = 2;
+	
 	//Channels
 	public static final int driveTalonLeft1Channel = 18;
 	public static final int driveTalonLeft2Channel = 17;
@@ -12,19 +17,16 @@ public class Constants {
 	public static final int driveTalonRight1Channel = 11;
 	public static final int driveTalonRight2Channel = 12;
 	public static final int driveTalonRight3Channel = 13;
-	public static final int driveSolenoidLeftChannel = 0;
-	public static final int driveSolenoidRightChannel = 1;
+	public static final int driveSolenoidChannel = 6;
 
 	public static final int intakeChannel = 10;
-	public static final int intakeLimitSwitchChannel = 33;//TODO real limit Switch to be added
 
 	public static final int armatureChannel = 19;
-	public static final int armatureLimitSwitchChannel = 34;//TODO real limit Switch to be added
+	
 	
 	public static final int shooterTalonLeftChannel = 15;
 	public static final int shooterTalonRightChannel = 14;
-	public static final int shooterSolenoidLeftChannel = 2;
-	public static final int shooterSolenoidRightChannel = 3;
+	public static final int shooterSolenoidChannel = 7;
 	
 	//Computation Constants
 	public static final double driveEncoderToFeet = 1; //TODO get this value
