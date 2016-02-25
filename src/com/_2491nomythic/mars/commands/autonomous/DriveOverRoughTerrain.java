38a49df0bd6f1bb82e3027a25f5226dc61def1ba
Adrian <awkirchner@gmail.com>
2016-02-25 17:22:11 -0600

    added drive over rough terrain autonomous

---
+++ src/com/_2491nomythic/mars/commands/autonomous/DriveOverRoughTerrain.java
@@ -0,0 +1,42 @@
+package com._2491nomythic.mars.commands.autonomous;
+
+import com._2491nomythic.mars.commands.drivetrain.DriveToPosition;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveOverRoughTerrain extends Command {
+	DriveToPosition drive13Feet;
+
+    public DriveOverRoughTerrain() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	drive13Feet = new DriveToPosition(13, 0.7);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	drive13Feet.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return (!drive13Feet.isRunning());
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	drive13Feet.cancel();
+    }
+}
