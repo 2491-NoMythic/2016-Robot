2b1d4ab02a153b563a67df69f366390e7c8a218a
Adrian <awkirchner@gmail.com>
2016-02-25 18:57:20 -0600

    edited THE AMAZING BOOLEAN OF AMAZINGNESS

--- src/com/_2491nomythic/mars/commands/UpdateDriverstation.java
+++ src/com/_2491nomythic/mars/commands/UpdateDriverstation.java
@@ -31,7 +31,7 @@ public class UpdateDriverstation extends CommandBase {
     protected void execute() {
     	if (timer.get() > nextRun) {
 			nextRun = nextRun + 0.1;
-//			SmartDashboard.putBoolean("Tower in range: ", (Math.abs(Constants.visionCenterXValue - grip.getCenterX()[0]) < Constants.acceptableVisionCenterXDifference) && (Math.abs(Constants.visionCenterYValue - grip.getCenterY()[0]) < Constants.acceptableVisionCenterYDifference) && (Math.abs(Constants.visionAreaValue - grip.getArea()[0]) < Constants.acceptableVisionAreaDifference) && (Math.abs(Constants.visionHeightValue - grip.getHeight()[0]) < Constants.acceptableVisionHeightDifference) && (Math.abs(Constants.visionWidthValue - grip.getWidth()[0]) < Constants.acceptableVisionWidthDifference));
+//			SmartDashboard.putBoolean("Tower in range: ", (Math.abs(Constants.visionCenterXValue - grip.getCenterX()[0]) < Constants.acceptableVisionCenterXDifference) && (Math.abs(Constants.visionCenterYValue - grip.getCenterY()[0]) < Constants.acceptableVisionCenterYDifference) && (Math.abs(Constants.visionAreaValue - grip.getArea()[0]) < Constants.acceptableVisionAreaDifference));
 			Variables.shooterSpeed = SmartDashboard.getNumber("Shooter Speed");
     	}
     }
