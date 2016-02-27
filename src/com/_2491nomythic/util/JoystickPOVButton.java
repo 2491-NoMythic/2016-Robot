10ffe466f832e2fd7c4ba68c7cae2c90b058d61f
Adrian <awkirchner@gmail.com>
2016-02-27 13:19:33 -0600

    deleted periods in JoystickPOVButton javadocs

--- src/com/_2491nomythic/util/JoystickPOVButton.java
+++ src/com/_2491nomythic/util/JoystickPOVButton.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
- * Create a joystick button for triggering commands using the POV.
+ * Create a joystick button for triggering commands using the POV
  */
 public class JoystickPOVButton extends Button {
 	
@@ -12,7 +12,7 @@ public class JoystickPOVButton extends Button {
 	int m_POVNumber;
 	
 	/**
-	 * Create a joystick button for triggering commands using the POV.
+	 * Create a joystick button for triggering commands using the POV
 	 * @param joystick The GenericHID object that has the button (e.g. Joystick, KinectStick, etc)
 	 * @param POVNumber The POV number
 	 */
