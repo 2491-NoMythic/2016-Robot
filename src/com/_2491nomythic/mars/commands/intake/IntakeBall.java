b04ba685ea40534d46498f62a23e46dabb5389ef
Adrian <awkirchner@gmail.com>
2016-03-01 19:02:12 -0600

    re-added the limit switch IntakeBall code

--- src/com/_2491nomythic/mars/commands/intake/IntakeBall.java
+++ src/com/_2491nomythic/mars/commands/intake/IntakeBall.java
@@ -29,8 +29,7 @@ public class IntakeBall extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        //return intake.getLimitSwitch1Value() || intake.getLimitSwitch2Value();
-    	return false;
+        return intake.getLimitSwitch1Value() || intake.getLimitSwitch2Value();
     }
 
     // Called once after isFinished returns true
