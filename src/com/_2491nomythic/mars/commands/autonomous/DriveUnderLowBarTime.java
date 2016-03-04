7cdca21f9aca194b2474ea16fe1638ee26828fed
Lenovo 4 <2491nomythic@gmail.com>
2016-03-04 12:59:46 -0600

    Added lower armature to drive under low bar

--- src/com/_2491nomythic/mars/commands/autonomous/DriveUnderLowBarTime.java
+++ src/com/_2491nomythic/mars/commands/autonomous/DriveUnderLowBarTime.java
@@ -1,5 +1,6 @@
 package com._2491nomythic.mars.commands.autonomous;
 
+import com._2491nomythic.mars.commands.armature.LowerArmatureTime;
 import com._2491nomythic.mars.commands.drivetrain.DriveTime;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -10,6 +11,8 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriveUnderLowBarTime extends Command {
 	
 	DriveTime drive5SecsForeward;
+	LowerArmatureTime lowerArmature;
+	boolean hasRun;
 	
 	/**
 	 * An autonomous that drives under the low bar, starting from directly in front of it on the starting line (based on time)
@@ -18,15 +21,22 @@ public class DriveUnderLowBarTime extends Command {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	drive5SecsForeward = new DriveTime(5, 0.7);
+    	lowerArmature = new LowerArmatureTime(0.5, 2); //takes in positive power
+    	hasRun = false;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	drive5SecsForeward.start();
+    	lowerArmature.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	if(!lowerArmature.isRunning() && !hasRun) {
+    		drive5SecsForeward.start();
+    		hasRun = true;
+    	}
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
