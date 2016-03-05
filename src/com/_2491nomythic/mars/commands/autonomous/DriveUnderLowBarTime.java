f9e0eaa7214f34c39af86305e973850680244ec8
Adrian <awkirchner@gmail.com>
2016-03-04 21:41:01 -0600

    fixed DriveUnderLowBarTime

--- src/com/_2491nomythic/mars/commands/autonomous/DriveUnderLowBarTime.java
+++ src/com/_2491nomythic/mars/commands/autonomous/DriveUnderLowBarTime.java
@@ -3,6 +3,7 @@ package com._2491nomythic.mars.commands.autonomous;
 import com._2491nomythic.mars.commands.armature.LowerArmatureTime;
 import com._2491nomythic.mars.commands.drivetrain.DriveTime;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -12,6 +13,7 @@ public class DriveUnderLowBarTime extends Command {
 	
 	DriveTime drive5SecsForeward;
 	LowerArmatureTime lowerArmature;
+	Timer timer;
 	boolean hasRun;
 	
 	/**
@@ -22,18 +24,19 @@ public class DriveUnderLowBarTime extends Command {
         // eg. requires(chassis);
     	drive5SecsForeward = new DriveTime(5, 0.7);
     	lowerArmature = new LowerArmatureTime(0.5, 0.8); //takes in positive power
+    	timer = new Timer();
     	hasRun = false;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	lowerArmature.start();
+    	drive5SecsForeward.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(!lowerArmature.isRunning() && !hasRun) {
-    		drive5SecsForeward.start();
+    	if(timer.get() > 1 && !hasRun) {
+    		lowerArmature.start();
     		hasRun = true;
     	}
     	
@@ -52,5 +55,6 @@ public class DriveUnderLowBarTime extends Command {
     // subsystems is scheduled to run
     protected void interrupted() {
     	drive5SecsForeward.cancel();
+    	lowerArmature.cancel();
     }
 }
