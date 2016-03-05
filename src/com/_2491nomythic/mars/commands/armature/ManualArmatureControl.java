a52f9ed166303f604a35f07152fcec0d9f314d6a
Lenovo_3 <2491nomythic@gmail.com>
2016-03-04 21:24:29 -0600

    Commented limit switch control

--- src/com/_2491nomythic/mars/commands/armature/ManualArmatureControl.java
+++ src/com/_2491nomythic/mars/commands/armature/ManualArmatureControl.java
@@ -43,9 +43,10 @@ public class ManualArmatureControl extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-//    	if(armature.armLimitSwitch()) {
+//    	if(armature.getLimitSwitch()) {
 //    		armature.set(0);
 //    	}
+    	System.out.println(armature.getEncoderPosition());
     }
 
     // Make this return true when this Command no longer needs to run execute()
