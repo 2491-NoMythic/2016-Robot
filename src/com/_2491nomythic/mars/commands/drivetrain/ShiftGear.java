2e8eb2075f47edf896863f604aa6874ad4bff183
Lenovo_3 <2491nomythic@gmail.com>
2016-03-04 21:22:25 -0600

    Regular shift command made

---
+++ src/com/_2491nomythic/mars/commands/drivetrain/ShiftGear.java
@@ -0,0 +1,41 @@
+package com._2491nomythic.mars.commands.drivetrain;
+
+import com._2491nomythic.mars.commands.CommandBase;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ShiftGear extends CommandBase {
+
+    public ShiftGear() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	drivetrain.shiftToHighGear();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	drivetrain.shiftToLowGear();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	drivetrain.shiftToLowGear();
+    }
+}
