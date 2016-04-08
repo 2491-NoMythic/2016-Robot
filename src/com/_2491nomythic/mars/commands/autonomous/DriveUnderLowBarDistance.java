ac715f104d95fb61f75e01a6776b8d4d141485c5
Lenovo_3 <2491nomythic@gmail.com>
2016-04-08 08:23:51 -0500

    Changed name in driveUnderLowBarDistance

--- src/com/_2491nomythic/mars/commands/autonomous/DriveUnderLowBarDistance.java
+++ src/com/_2491nomythic/mars/commands/autonomous/DriveUnderLowBarDistance.java
@@ -10,7 +10,7 @@ import com._2491nomythic.mars.commands.drivetrain.DriveStraightToPosition;
  * An autonomous that drives under the low bar, starting from directly in front of it on the starting line (based on encoder values)
  */
 public class DriveUnderLowBarDistance extends CommandBase {
-	DriveStraightToPosition drive7Feet;
+	DriveStraightToPosition drive14Feet;
 	ArmaturePositionSet moveArmToLowBarConfiguration;
 	ArmatureTime lowerArmature;
 	boolean hasRun;
@@ -21,7 +21,7 @@ public class DriveUnderLowBarDistance extends CommandBase {
 	public DriveUnderLowBarDistance() {
 		// Use requires() here to declare subsystem dependencies
 		// eg. requires(chassis);
-		drive7Feet = new DriveStraightToPosition(-14, -0.8);
+		drive14Feet = new DriveStraightToPosition(-14, -0.8);
 		lowerArmature = new ArmatureTime(0.5, 0.8); // takes in positive power
 //		moveArmToLowBarConfiguration = new ArmaturePositionSet(Constants.armatureLowBarDifference - armature.getEncoderPosition());
 	}
@@ -29,7 +29,7 @@ public class DriveUnderLowBarDistance extends CommandBase {
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		hasRun = false;
-		drive7Feet.start();
+		drive14Feet.start();
 	}
 	
 	// Called repeatedly when this Command is scheduled to run
@@ -43,7 +43,7 @@ public class DriveUnderLowBarDistance extends CommandBase {
 	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return (!drive7Feet.isRunning() && !lowerArmature.isRunning());
+		return (!drive14Feet.isRunning() && !lowerArmature.isRunning());
 	}
 	
 	// Called once after isFinished returns true
@@ -53,7 +53,7 @@ public class DriveUnderLowBarDistance extends CommandBase {
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
-		drive7Feet.cancel();
+		drive14Feet.cancel();
 		moveArmToLowBarConfiguration.cancel();
 		lowerArmature.cancel();
 	}
